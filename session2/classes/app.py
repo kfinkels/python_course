0615c55ff36eb21be7453908c3c655224d4ddba1
Yinon D. Nahamu <yinondn@gmail.com>
2019-05-21 14:41:16 +0300

    add download and compress

--- session2/classes/app.py
+++ session2/classes/app.py
@@ -1,3 +1,6 @@
+import shutil
+from os import makedirs
+from urllib import request
 from session2.classes.compressors.gzip_compressor import GzipCompressor
 from session2.classes.compressors.rar_compressor import RarCompressor
 from session2.classes.compressors.zip_compressor import ZipCompressor
@@ -7,21 +10,29 @@ from session2.classes.transfer_adapters.s3_adapter import S3Adapter
 
 
 class Supplier:
-    def __init__(self, address, compression_type, transfer_type, transfer_params):
-        self.address = address
+    def __init__(self, addresses, compression_type, transfer_type, transfer_params):
+        self.addresses = addresses
         self.compression_type = compression_type
         self.transfer_type = transfer_type
         self.transfer_params = transfer_params
 
     def supply(self):
-        local_path = self.__download_file()
-        compressed_local_path = self.__compress_file(local_path)
+        local_files = self.__download_files()
+        compressed_local_path = self.__compress_file(local_files[0])
         self.__transfer_file(compressed_local_path)
 
-    def __download_file(self):
-        print(f"Downloading from {self.address}")
-        local_path = f"\\tmp\\{self.address.split('/')[-1]}"
-        return local_path
+    def __download_files(self):
+        local_files = []
+
+        for address in self.addresses:
+            print(f"Downloading from {address}")
+            local_path = f"tmp/{address.split('/')[-1]}"
+
+            with request.urlopen(address) as response, open(local_path, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+                local_files.append(local_path)
+
+        return local_files
 
     def __compress_file(self, path):
         if self.compression_type == CompressionType.ZIP:
@@ -39,8 +50,13 @@ class Supplier:
 
 
 if __name__ == '__main__':
+    try:
+        makedirs("tmp")
+    except:
+        pass
+
     supplier = Supplier(
-        address="https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv",
+        addresses=["https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv"],
         compression_type=CompressionType.ZIP,
         transfer_type=TransferType.FTP,
         transfer_params=dict(
--- session2/classes/compressors/zip_compressor.py
+++ session2/classes/compressors/zip_compressor.py
@@ -1,7 +1,12 @@
+import zipfile
+
 from session2.classes.compressors.base import CompressorBase
 
 
 class ZipCompressor(CompressorBase):
     def compress(self):
         print(f"Compressing {self.path} to ZIP")
-        return f"{self.path}.zip"
+
+        output_file_path = self.path.replace(".csv", ".zip")
+        output_file = zipfile.ZipFile(output_file_path, mode='w')
+        output_file.write(self.path)
