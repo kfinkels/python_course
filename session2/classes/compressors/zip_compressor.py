77e0452708480eab17ea582824ae2245a620e718
Yinon D. Nahamu <yinondn@gmail.com>
2019-05-21 15:53:50 +0300

    add

--- session2/classes/compressors/zip_compressor.py
+++ session2/classes/compressors/zip_compressor.py
@@ -5,8 +5,16 @@ from session2.classes.compressors.base import CompressorBase
 
 class ZipCompressor(CompressorBase):
     def __init__(self, output_path):
-        self.output_file = zipfile.ZipFile(output_path, mode='w')
+        self.output_path = output_path
+        self.output_file = None
 
     def compress(self, path):
         print(f"Compressing {path} to ZIP")
         self.output_file.write(path)
+
+    def __enter__(self):
+        self.output_file = zipfile.ZipFile(self.output_path, mode='w')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.output_file.close()
