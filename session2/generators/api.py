fc9a5894a384f284c6cfa45b913061912782871e
Yinon D. Nahamu <yinondn@gmail.com>
2019-06-12 01:11:58 +0300

    2

---
+++ session2/generators/api.py
@@ -0,0 +1,17 @@
+class Api:
+    def run_first(self):
+        pass
+
+    def run_second(self):
+        pass
+
+    def run_last(self):
+        pass
+
+
+def api():
+    a()
+    yield
+    b()
+    yield
+    c()
