501823c6a8a4f66624d285907cad300d9f0fb6d9
Yinon D. Nahamu <yinondn@gmail.com>
2019-06-12 10:51:13 +0300

    new

--- session2/decorators/dec.py
+++ session2/decorators/dec.py
@@ -1,19 +1,21 @@
 from time import time
 
 
-def timer(func):
-    def f(x, y):
-        before = time()
+def divide(x, y=1):
+    return x / y
+
+
+def logger(func):
+    def wrapper(x, y=1):
+        print(f"starting {func.__name__} at {time()}")
         value = func(x, y)
-        after = time()
-        print("elapsed", after - before)
+        print(f"finished {func.__name__} at {time()}")
         return value
-    return f
+    return wrapper
 
 
-def add(x, y):
-    return x + y
+divide = logger(divide)
+print(divide(10, 20))
+print(divide(100, 100))
+print(divide(-5, 15))
 
-
-add = timer(add)
-print(add(10, 20))
